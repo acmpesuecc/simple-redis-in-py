afc813df9953769a6a45cb6138f04bfd5c2260ae
Reema Sarkar <145077817+reema-s1@users.noreply.github.com>
2024-10-17 22:51:56 +0530

    basic one

---
+++ client3.py
@@ -0,0 +1,61 @@
+import socket
+import json
+
+class Client:
+    def __init__(self,host='127.0.0.1', port=31337):
+        self.host= host
+        self.port= port
+
+    def send_request(self,action, key=None, value=None, keys=None, items=None):
+        request = {'action': action} 
+        if key is not None:
+            request['key'] = key
+        if value is not None:
+            request['value'] = value
+        if keys is not None:
+            request['keys'] = keys
+        if items is not None:
+            request['items']= items
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((self.host, self.port))
+            client_socket.send(json.dumps(request).encode('utf-8'))
+            response = client_socket.recv(1024).decode('utf-8')
+            return json.loads(response)
+
+    def run(self):
+        print("currently in client - enter 'bye' to quit:c")
+        while True:
+            command = input("\nenter command (example - SET key value, GET key, DELETE key, FLUSH, MSET key1=value1,key2=value2, MGET key1,key2): ")
+            if command.lower()== 'bye':
+                break
+            
+            parts = command.split()
+            action= parts[0].upper()
+
+            if action == 'SET':
+                key= parts[1]
+                value = parts[2]
+                response = self.send_request(action,key=key, value=value)
+            elif action == 'GET':
+                key = parts[1]
+                response = self.send_request(action,key=key)
+            elif action == 'DELETE':
+                key= parts[1]
+                response = self.send_request(action, key=key)
+            elif action == 'FLUSH':
+                response = self.send_request(action)
+            elif action== 'MSET':
+                items = dict(item.split('=') for item in parts[1].split(','))
+                response = self.send_request(action,items=items)
+            elif action == 'MGET':
+                keys = parts[1].split(',')
+                response = self.send_request(action,keys=keys)
+            else:
+                response= 'Unknown command'
+
+            print(f'response: {response}')
+
+if __name__ == '__main__':
+    client= Client()
+    client.run()
---
+++ server3.py
@@ -0,0 +1,74 @@
+import socket
+import json
+
+#starting your server
+class Server:
+    def __init__(self, host='127.0.0.1', port=31337): #loopback ip address - local host(specifically for testing connections - doesnt connect to outside internet - always refers to own device), #port - door that lets data in/out (can pick any number btw 1024-65535(0-1023 is reserved for system proc - http,ftp))
+        self.store= {} #memory
+        self.host= host
+        self.port= port
+
+# adding your methods and responses for clarity
+    def set(self,key,value): 
+        self.store[key] = value 
+        return f'stored {key}'
+
+    def get(self,key):
+        return self.store.get(key,None)
+
+    def delete(self,key):
+        if key in self.store:
+            del self.store[key]
+            return f'deleted {key}'
+        return None
+
+    def flush(self):
+        self.store.clear()
+        return 'cleared all data ruh roh'
+
+    def mget(self, keys):
+        return {key: self.store.get(key) for key in keys} 
+
+    def mset(self,items):
+        for key, value in items.items():
+            self.store[key] = value
+        return 'stored multiple values :O'
+
+#here we have to manage said methods w a client socket in a server
+    def handle_client(self, client_socket):
+        request = client_socket.recv(1024).decode('utf-8') #decode to conv to string format(json) + serialization
+        command = json.loads(request) 
+        #this takes a JSON-encoded string as i/p and parses it into a py obj (dict) , request has the raw data from socket AS a utf-8 encoded string, and command becomes our py dict
+
+        if command['action'] == 'SET':
+            response= self.set(command['key'], command['value'])
+        elif command['action'] == 'GET':
+            response = self.get(command['key'])
+        elif command['action'] == 'DELETE':
+            response= self.delete(command['key'])
+        elif command['action'] == 'FLUSH':
+            response = self.flush()
+        elif command['action'] == 'MGET':
+            response = self.mget(command['keys'])
+        elif command['action'] == 'MSET':
+            response = self.mset(command['items'])
+        else:
+            response = 'unknown command pls try again'
+
+        client_socket.send(json.dumps(response).encode('utf-8'))
+        client_socket.close()
+
+    def run(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket obj, AF_INET - IPv4 protocol, sock_stream -  TCP protocol 
+        server_socket.bind((self.host, self.port)) #binds the socket to host and port to listen to incoming connections
+        server_socket.listen(5)
+        print(f'YAHOO server is running on {self.host}:{self.port}')
+
+        while True: #logging(?) because will go insane otherwise
+            client_socket,addr = server_socket.accept()
+            print(f'connection from {addr}')
+            self.handle_client(client_socket)
+
+if __name__ == '__main__': #code only runs when script is executed directly, NOT IMPORTED
+    server= Server()
+    server.run()
